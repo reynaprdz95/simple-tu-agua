a4a37ae73f42b2edadc72da013b6f696dd312164
reynaprdz95 <reynaprdz95@gmail.com>
2026-06-14 18:53:34 -0600

    version 1

---
+++ site-config.js
@@ -0,0 +1,11 @@
+window.SIMPLE_CONFIG = {
+  crmEndpoint: "/api/lead",
+  coverageEndpoint: "/api/coverage-interest",
+  appUrl: "",
+  appStoreUrl: "https://apps.apple.com/mx/app/simple-agua-para-beber/id1619997635",
+  googlePlayUrl: "https://play.google.com/store/apps/details?id=com.simple.water&hl=es_MX&gl=MX",
+  whatsappNumber: "525580802486",
+  instagramUrl: "https://www.instagram.com/simpletuagua/",
+  facebookUrl: "",
+  linkedinUrl: ""
+};
